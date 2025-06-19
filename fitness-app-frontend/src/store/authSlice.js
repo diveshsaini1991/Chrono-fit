435ef60f080441f8dd40ecb68f6c23a60fd6694d
Divesh Saini <diveshsaini1991@gmail.com>
2025-06-19 17:51:27 +0530

    Frontend(Authantication): PKCE Authantication workflow is created

---
+++ fitness-app-frontend/src/store/authSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { 
+    user:JSON.parse(localStorage.getItem("user"))|| null,
+    token: localStorage.getItem("token") || null,
+    userId: localStorage.getItem("userId") || null,
+ }
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state,action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.userId = action.payload.user.sub;
+
+        localStorage.setItem("token", action.payload.token);
+        localStorage.setItem("user", JSON.stringify(action.payload.user));
+        localStorage.setItem("userId", action.payload.user.sub);
+
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.userId = null;
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      localStorage.removeItem("userId");
+
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
