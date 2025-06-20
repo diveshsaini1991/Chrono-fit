829edbae4f09218a33e43886a9fb44e9b54b03f4
Divesh Saini <diveshsaini1991@gmail.com>
2025-06-20 18:00:45 +0530

    Frontend(Activity): Activity form created

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "react-oauth2-code-pkce";
 import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
 import { setCredentials } from "./store/authSlice";
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail";
+
+const ActivityPage = ()=>{
+  return (<Box component="section" sx={{p: 2, border: "1px dashed grey"}}>
+    <ActivityForm onActivitiesAdded = {()=> window.location.reload()}/>
+    <ActivityList/>
+  </Box>);
+}
 
 function App() {
   const { token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext);
@@ -25,9 +35,14 @@ function App() {
                 logIn()
               }}>Login</Button>
             ):(
-              <div>
-                <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-              </div>
+              <Box component="section" sx={{p: 2, border: "1px dashed grey"}}>
+                <Routes>
+                  <Route path="/activities" element={<ActivityPage/>}/>
+                  <Route path="/activities/:id" element={<ActivityDetail/>}/>
+                  
+                  <Route path="/" element={token?<Navigate to="/activities" replace/>:<div>Welcome Please Login</div>}/>
+                </Routes>
+              </Box>
             )}
     </Router>
   )
---
+++ fitness-app-frontend/src/components/ActivityForm.jsx
@@ -0,0 +1,59 @@
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
+import React, { useState } from 'react'
+
+
+const ActivityForm = ({onActivityAdded}) => {
+    
+    const [activity, setActivity] = useState({
+        type: "RUNNING",
+        duration: "",
+        caloriesBurned: "",
+        additionalMetrics: {}
+    })
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        try{
+            // await addActivity(activity);
+            onActivityAdded();
+            setActivity({ type: "RUNNING", duration: "", caloriesBurned: ""})
+        }catch(error){
+    
+        }
+    }
+    
+  return (
+    <Box component="form" sx={{mb: 4}} onSubmit={handleSubmit}>
+        <FormControl fullWidth sx={{mb:2}}>
+            <InputLabel>Activity Type</InputLabel>
+            <Select
+                value={activity.type}
+                onChange={(e)=>setActivity({...activity,type:e.target.value})}>
+                    <MenuItem value="RUNNING">Running</MenuItem>
+                    <MenuItem value="WALKING">Walking</MenuItem>
+                    <MenuItem value="CYCLING">Cycling</MenuItem>
+                    <MenuItem value="OTHER">Other</MenuItem>
+            </Select>
+            </FormControl>
+            <TextField 
+                fullWidth
+                sx={{mb:2}}
+                type='number'
+                label = "Duration (Mins)"
+                value={activity.duration}
+                onChange={(e)=>setActivity({...activity,duration:e.target.value})}/>
+
+            <TextField 
+                fullWidth
+                sx={{mb:2}}
+                type='number'
+                label = "Calories Burned"
+                value={activity.caloriesBurned}
+                onChange={(e)=>setActivity({...activity,caloriesBurned:e.target.value})}/>
+
+                <Button type="submit" variant="contained">Add Activity</Button>
+    </Box>
+  )
+}
+
+export default ActivityForm
